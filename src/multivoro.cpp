8429123f422135fac9ed5b8f32b9ef1171edf6ad
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2024-03-04 17:56:56 +0100

    First Success

--- src/multivoro.cpp
+++ src/multivoro.cpp
@@ -1,6 +1,27 @@
 #include <nanobind/nanobind.h>
 #include "voro++.hh"
 
+namespace nb = nanobind;
+
 NB_MODULE(_multivoro, m) {
-    m.def("hello", []() { return "Hello world!"; });
+    nb::class_<voro::container_base_3d>(m, "container_base_3d")
+        .def(
+            nb::init<
+                double, // ax
+                double, // bx
+                double, // ay
+                double, // by
+                double, // az
+                double, // bz
+                int,    // nx
+                int,    // ny
+                int,    // nz
+                bool,   // x_prd
+                bool,   // y_prd
+                bool,   // z_prd
+                int,    // init_mem
+                int,    // ps_
+                int     // nt_
+            >()
+        );
 }
---
+++ src/multivoro/__init__.py
@@ -0,0 +1,5 @@
+from ._multivoro import container_base_3d
+
+
+def compute_voronoi_diagram(points, boundaries=None, periodicity=False, radii=None, nthreads=1):
+    pass
