f22019ff36cd544a2f0c72361d4c4bd2220386ad
Eleftherios Zisis <eleftherios.zisis@epfl.ch>
2024-05-06 20:41:35 +0200

    Try

--- ci/build_libomp.sh
+++ ci/build_libomp.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-build_dir=${DIR}/libs_build
+BUILD_DIR=${DIR}/libs_build
 
 git clone --depth 1 --branch llvmorg-11.1.0 https://github.com/llvm/llvm-project
 pushd llvm-project/openmp
 mkdir build
 cd build
-cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="${build_dir}" -DCMAKE_MACOSX_RPATH="${build_dir}/lib" ..
+cmake -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_INSTALL_PREFIX="${BUILD_DIR}" -DCMAKE_MACOSX_RPATH="${BUILD_DIR}/lib" ..
 make
 make install
 
